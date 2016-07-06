e54edc4addc1c46dc395581c197bc673cd93112d
manoj-apare <manojapare@gmail.com>
2016-07-06 12:55:25 +0530

    Update script to post entity from processed xml.

--- vb_vufind/Rest_api/import-xsl.php
+++ vb_vufind/Rest_api/import-xsl.php
@@ -8,7 +8,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://nvli.local/rest/session/token",
+  CURLOPT_URL => "http://188.166.208.4/drupal8/rest/session/token",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -30,47 +30,50 @@ if ($err) {
   echo "cURL Error #:" . $err; exit;
 }
 
+$harvest_dir = '/usr/local/vufind/local/harvest/DSpace/processed';
+$harvested_files = glob("$harvest_dir/*.xml");
+
 // Get xml template to post from xml data using xsl.
 
 $xslDoc = new DOMDocument();
 $xslDoc->load("drupal-dspace.xsl");
+foreach ($harvested_files as $oai_dc) {
+  $xmlDoc = new DOMDocument();
+  $xmlDoc->load($oai_dc);
 
-$xmlDoc = new DOMDocument();
-$xmlDoc->load("1467108207_ir_100_18429.xml");
+  $proc = new XSLTProcessor();
+  $proc->importStylesheet($xslDoc);
+  $input_xml = $proc->transformToXml($xmlDoc);
 
-$proc = new XSLTProcessor();
-$proc->importStylesheet($xslDoc);
-$input_xml = $proc->transformToXml($xmlDoc);
+  // Post it using curl.
 
-// Post it using curl.
-$url = "http://nvli.local/entity/solr_annotation?_format=xml";
+  $curl = curl_init();
 
-$curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://188.166.208.4/drupal8/entity/solr_annotation?_format=xml",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $input_xml,
+    CURLOPT_HTTPHEADER => array(
+      "authorization: Basic YWRtaW46VmFsdWViMHVuZEBudmxp",
+      "cache-control: no-cache",
+      "content-type: application/xml",
+      "x-csrf-token: $x_csrf_token"
+    ),
+  ));
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://nvli.local/entity/solr_annotation?_format=xml",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "<request>\n  <solr_doc_id>\n    <value>ir-100-test</value>\n  </solr_doc_id>\n  <annotation>\n    <value>test</value>\n  </annotation>\n</request>",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Basic bnZsaTpudmxp",
-    "cache-control: no-cache",
-    "content-type: application/xml",
-    "x-csrf-token: $x_csrf_token"
-  ),
-));
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  curl_close($curl);
 
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
 }
