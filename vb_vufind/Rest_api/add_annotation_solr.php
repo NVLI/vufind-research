aef2cf0b7564556bbe9908b3b17225dbcaa1e005
aditya_anurag <adityaanurag21@gmail.com>
2016-07-27 19:07:48 +0530

    updated script file to update annotation

--- vb_vufind/Rest_api/add_annotation_solr.php
+++ vb_vufind/Rest_api/add_annotation_solr.php
@@ -1,22 +1,26 @@
 <?php
 $curl = curl_init();
-$base_url = 'http://dev-d8.local';
+$base_url = 'http://dev-nvli.iitb.ac.in';
+
 curl_setopt_array($curl, array(
   CURLOPT_URL => $base_url."/nvli/count?_format=xml",
-  CURLOPT_RETURNTRANSFER => TRUE,
+  CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
-    "authorization: Basic YWRtaW46YWRtaW4=",
-    "cache-control: no-cache"
+    "authorization: Basic YWRtaW46cGFzc3dvcmQ=",
+    "cache-control: no-cache",
+    "content-type: application/xml",
+    "x-csrf-token: _J2YxnzYYgl1KkZL3XvRrsNdshwutgR9ItPnWKZsoYE"
   ),
 ));
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
+
 curl_close($curl);
 
 if ($err) {
@@ -28,7 +32,7 @@ else {
   for ($i = 0; $i < ceil($count / 100); $i++) {
     curl_setopt_array($curl, array(
       CURLOPT_URL => $base_url."/nvli/add-annotation?_format=xml&offset=" . ($i * 100) . "&limit=100",
-      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
       CURLOPT_TIMEOUT => 30,
@@ -36,19 +40,22 @@ else {
       CURLOPT_CUSTOMREQUEST => "GET",
       CURLOPT_POSTFIELDS => "",
       CURLOPT_HTTPHEADER => array(
-        "authorization: Basic YWRtaW46YWRtaW4=",
-        "cache-control: no-cache"
+        "authorization: Basic YWRtaW46cGFzc3dvcmQ=",
+        "cache-control: no-cache",
+        "content-type: application/xml",
+        "x-csrf-token: _J2YxnzYYgl1KkZL3XvRrsNdshwutgR9ItPnWKZsoYE"
       ),
     ));
     $response = curl_exec($curl);
     $err = curl_error($curl);
+
+    curl_close($curl);
+
     if ($err) {
       echo "cURL Error #:" . $err;
-    }
-    else {
+    } else {
       $result = simplexml_load_string($response);
-      echo 'Record Processed : '.($result->success+$result->fail+$result->exist).' Success Record : '.$result->success.' Fail Record : '.$result->fail.' Exist Record : '.$result->exist;
+      echo 'Record Processed : '.($result->success+$result->fail).' Success Record : '.$result->success.' Fail Record : '.$result->fail;
     }
   }
-  curl_close($curl);
 }
