3860585ba0bc57232cdba18eb5eedcd4cef990a1
manoj-apare <manojapare@gmail.com>
2016-08-02 16:09:33 +0530

    Add http password.

--- vb_vufind/Rest_api/import-drupal-xsl.php
+++ vb_vufind/Rest_api/import-drupal-xsl.php
@@ -6,7 +6,8 @@
 
 $vufind_home = getenv('VUFIND_HOME');
 $oai_ini_path = $vufind_home . '/harvest/oai.ini';
-$nvli_drupal_host = 'http://188.166.216.52/d8';
+$http_password = 'admin:password@';
+$nvli_drupal_host = 'http://' . $http_password . 'dev-nvli.iitb.ac.in';
 $vufind_local_dir = getenv('VUFIND_LOCAL_DIR');
 
 // Load oai.ini
@@ -40,7 +41,8 @@ $err = curl_error($curl);
 curl_close($curl);
 
 if ($err) {
-  echo "cURL Error #:" . $err; exit;
+  echo "cURL Error #:" . $err;
+  exit;
 }
 
 foreach ($oai_pmh_list as $oai_pmh) {
@@ -73,13 +75,14 @@ foreach ($oai_pmh_list as $oai_pmh) {
 
     if ($err) {
       echo "cURL Error #:" . $err;
+      exit;
     }
     $response = simplexml_load_string($response);
     $file = fopen($oai_pmh_dir . '/drupal-harvest-export.log', 'a+');
     $processed_ids = array();
     foreach ($response  as $item) {
-      $processed_ids[] = $item->solr_doc_id;
-      fputs($file, $item->solr_doc_id . PHP_EOL);
+      $processed_ids[] = $item->field_solr_docid;
+      fputs($file, $item->field_solr_docid . PHP_EOL);
     }
   }
   else {
@@ -90,8 +93,10 @@ foreach ($oai_pmh_list as $oai_pmh) {
 
   // Get xml template to post from xml data using xsl.
   $xslDoc = new DOMDocument();
+  if (!file_exists(__DIR__ . "/xsl/drupal-" . strtolower($oai_pmh) . ".xsl")) {
+    continue;
+  }
   $xslDoc->load(__DIR__ . "/xsl/drupal-" . strtolower($oai_pmh) . ".xsl");
-  $i = 0;
   foreach ($harvested_files as $oai_dc) {
     $xmlDoc = new DOMDocument();
     $xmlDoc->load($oai_dc);
@@ -99,14 +104,14 @@ foreach ($oai_pmh_list as $oai_pmh) {
     $proc = new XSLTProcessor();
     $proc->importStylesheet($xslDoc);
     $input_xml = $proc->transformToXml($xmlDoc);
-    $cur_solr_doc_id = (array) simplexml_load_string($input_xml)->solr_doc_id->value;
+    $cur_solr_doc_id = (array) simplexml_load_string($input_xml)->field_solr_docid->value;
 
     if (!in_array($cur_solr_doc_id[0], $processed_ids)) {
       // Post it using curl.
       $curl = curl_init();
 
       curl_setopt_array($curl, array(
-        CURLOPT_URL => "$nvli_drupal_host/entity/nvli_resource_entity?_format=xml",
+        CURLOPT_URL => "$nvli_drupal_host/entity/node?_format=xml",
         CURLOPT_RETURNTRANSFER => TRUE,
         CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
@@ -115,7 +120,7 @@ foreach ($oai_pmh_list as $oai_pmh) {
         CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_POSTFIELDS => $input_xml,
         CURLOPT_HTTPHEADER => array(
-          "authorization: Basic YWRtaW46YWRtaW4=",
+          "authorization: Basic YWRtaW46cGFzc3dvcmQ=",
           "cache-control: no-cache",
           "content-type: application/xml",
           "x-csrf-token: $x_csrf_token"
@@ -135,7 +140,6 @@ foreach ($oai_pmh_list as $oai_pmh) {
         echo 'Created entity for solr_doc_id: ' . $cur_solr_doc_id[0] . ' in ' . $oai_pmh_dir . PHP_EOL;
         fputs($file, $cur_solr_doc_id[0] . PHP_EOL);
       }
-      if($i == 9) {exit;} $i++;
     }
   }
 }
